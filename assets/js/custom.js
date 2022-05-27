83b4b4f99245fd87121abbfb97140f8dc9bad864
abbasinfoicon <uid3@infoiconsoftware.com>
2022-05-27 16:02:01 +0530

    client feedback

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -167,6 +167,87 @@
 
         }
 
+        if ($('.notification_popup').length > 0) {
+
+            // notification_id
+            $(function () {
+                $('.notification_id').hover(function () {
+                    $('.notification_popup').addClass('show');
+                }, function () {
+                    $('.notification_popup').removeClass('show');
+                });
+            });
+            // notification_id
+
+        }
+
+
+        if ($('.edit_pro').length > 0) {
+
+            // notification_id
+            $('.edit_pro').click(function () {
+                $('.form_edit  input, .form_edit  textarea').removeAttr("disabled");
+            });
+            // notification_id
+        }
+
+        if ($('#phone').length > 0) {
+            // countery-list start
+
+            var telInput = $("#phone"),
+                errorMsg = $("#error-msg"),
+                validMsg = $("#valid-msg");
+
+            // initialise plugin
+            telInput.intlTelInput({
+
+                allowExtensions: true,
+                formatOnDisplay: true,
+                autoFormat: true,
+                autoHideDialCode: true,
+                autoPlaceholder: true,
+                defaultCountry: "auto",
+                ipinfoToken: "yolo",
+
+                nationalMode: false,
+                numberType: "MOBILE",
+                //onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+                preferredCountries: ['sa', 'ae', 'qa', 'om', 'bh', 'kw', 'ma'],
+                preventInvalidNumbers: true,
+                separateDialCode: true,
+                initialCountry: "auto",
+                geoIpLookup: function (callback) {
+                    $.get("http://ipinfo.io", function () { }, "jsonp").always(function (resp) {
+                        var countryCode = (resp && resp.country) ? resp.country : "";
+                        callback(countryCode);
+                    });
+                },
+                utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.9/js/utils.js"
+            });
+
+            var reset = function () {
+                telInput.removeClass("error");
+                errorMsg.addClass("hide");
+                validMsg.addClass("hide");
+            };
+
+            // on blur: validate
+            telInput.blur(function () {
+                reset();
+                if ($.trim(telInput.val())) {
+                    if (telInput.intlTelInput("isValidNumber")) {
+                        validMsg.removeClass("hide");
+                    } else {
+                        telInput.addClass("error");
+                        errorMsg.removeClass("hide");
+                    }
+                }
+            });
+
+            // on keyup / change flag: reset
+            telInput.on("keyup change", reset);
+            // countery-list end
+        }
 
 
         // WOW active
